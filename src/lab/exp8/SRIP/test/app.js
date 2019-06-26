0c581841c791cd088874c4090a612415778d4631
Ushasri Mavuri <ushasrimavuri1999@gmail.com>
2019-06-26 10:50:14 +0530

    added files

---
+++ src/lab/exp8/SRIP/test/app.js
@@ -0,0 +1,55 @@
+var content = document.createElement("div");
+content.classid.add("content");
+document.body.append(content);
+
+var boundingBox = document.createElement("h1");
+boundingBox.classList.add("boundingBox");
+boundingBox.textContent="Experimental Setup";
+content.append(boundingBox);
+
+var container = document.createElement("div");
+container.classList.add("container");
+document.body.append(container);
+
+var row = document.createElement("div");
+row.classList.add("row");
+container.append(row);
+
+var col-sm = document.createElement("div");
+col-sm.classList.add("col-sm-4");
+row.append(col-sm);
+
+var r1 = document.createElement("div");
+r1.classList.add("r1");
+col-sm.append(r1);
+
+var p = document.createElement("p");
+r1.append(p);
+
+div r2 = document.createElement("div");
+r2.classList.add("r2");
+col-sm.append(r2);
+
+var p = document.createElement("p");
+p.textContent="Protein Solution";
+r2.append(p);
+
+div r3 = document.createElement("div");
+r3.classList.add("r3");
+col-sm.append(r3);
+
+var p = document.createElement("p");
+p.textContent="Pippette";
+r3.append(p);
+
+div r4 = document.createElement("div");
+r4.classList.add("r4");
+col-sm.append(r4);
+
+var p = document.createElement("p");
+p.textContent="Cuvette";
+r4.append(p);
+
+var col-sm = document.createElement("div");
+col-sm.classList.add("col-sm-4");
+row.append(col-sm);
