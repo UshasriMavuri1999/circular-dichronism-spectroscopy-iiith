1ac72f3269f5dfd60259b5a931fb67064f2d99d1
Ushasri Mavuri <ushasrimavuri1999@gmail.com>
2019-09-04 19:35:59 +0530

    added

--- src/lab/exp8/SRIP/DATA/app.js
+++ src/lab/exp8/SRIP/DATA/app.js
@@ -89,3 +89,17 @@ spec.src="spectrometer.jpg";
   {
   document.getElementsByClassName('right1Img').style.position="absolute";
   }
+  function myMove() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
+      }
+    }
+  }
