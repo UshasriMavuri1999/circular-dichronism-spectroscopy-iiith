f1c871806ad76d254750c4ffb3b1c9a4ca1a3838
Ushasri Mavuri <ushasrimavuri1999@gmail.com>
2019-09-04 19:39:40 +0530

    added

--- src/lab/exp8/SRIP/DATA/app.js
+++ src/lab/exp8/SRIP/DATA/app.js
@@ -103,3 +103,28 @@ spec.src="spectrometer.jpg";
       }
     }
   }
+  setInterval(function() {
+  animate1();
+  animate2();
+  animate3();
+}, 20)
+function animate({timing, draw, duration}) {
+
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction goes from 0 to 1
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // calculate the current animation state
+    let progress = timing(timeFraction);
+
+    draw(progress); // draw it
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
